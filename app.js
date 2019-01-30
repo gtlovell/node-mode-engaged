6021b569344d3add35def4948c4a1f69a5b6b252
Garrett Lovell <seanhelmes@Garretts-MacBook-Pro-2.local>
2019-01-30 13:08:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
